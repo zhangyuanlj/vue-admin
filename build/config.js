7d22545d65d52e8bf0e320ebf792b70391b45342
zhangyuanlj <zhangyuanlj13@163.com>
2017-11-29 15:09:07 +0800

    Update config.js

--- build/config.js
+++ build/config.js
@@ -5,7 +5,7 @@ const config = {
     imgPath: "images",
     stylesPath: "styles",
     scriptsPath: "scripts",
-    projectTitle: "资产管理",
+    projectTitle: "后台管理系统",
     //组件服务器地址
     resourcesUrl: "http://10.101.0.130:8080/getList.php",
     //环境变量develop：开发，test：测试，product：线上
